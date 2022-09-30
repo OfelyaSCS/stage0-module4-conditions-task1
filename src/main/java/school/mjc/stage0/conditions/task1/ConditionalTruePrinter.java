8b10fcdf15d22302229f8fa96eb8849eca880ada
OfelyaSCS <sahakyanscofelya@gmail.com>
2022-09-30 23:21:43 +0400

    stage0-module4-conditions-task1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int a = 4, b = 5;
+        if (a < b) {
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.conditions.task1;
 
+import java.util.Scanner;
+
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        if(numberToBeDetermined>0){
+            System.out.println("Number is positive");
+    }else {
+            System.out.println("Number is negative");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int number=input.nextInt();
+
+        PositiveNumberDeterminer positiveNumberDeterminer=new PositiveNumberDeterminer();
+        positiveNumberDeterminer.isPositive(number);
     }
 }
